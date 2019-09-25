3dc7a2d96f36c909a989699c48f6fa7e71423439
Hui Wu <wuhui2014@stu.xjtu.edu.cn>
2019-09-25 21:31:15 +0800

    client test

--- client.c
+++ client.c
@@ -7,69 +7,45 @@
 #include <unistd.h>
 
 
-int connect_controller(int port)
+int main()
 {
+    unsigned char send_buf[] = "abcde";
+    unsigned char recv_buf[100];
+
     struct sockaddr_in my_addr;
     // memset(&my_addr, 0, sizeof(my_addr));
     my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(port);
+    my_addr.sin_port = htons(6788);
     my_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-
-
     // create socket
-    int sockfd  = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0)
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
     {
         printf("create socket failed\n");
         return -1;
     }
 
     // connect
-    if(connect(sockfd, (struct sockaddr*)&my_addr, sizeof(my_addr)) < 0)
+    if (connect(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0)
     {
         printf("connect failed\n");
         return -1;
     }
-    return sockfd;
-}
 
-int talk_controller(int sockfd, unsigned char* send_msg)
-{
-    if(send(sockfd, send_msg, sizeof(send_msg), 0) < 0)
+    while (1)
     {
-        printf("send failed\n");
-        return -1;
-    }
-
-    // recv
-    unsigned char recv_msg[100];
-    if(recv(sockfd, recv_msg, sizeof(recv_msg), 0) > 0)
-    {
-        printf("recv: %s\n", recv_msg);
-    }else{
-        printf("recv failed\n");
-    }
-
-    return 0;
-}
-
-int main() 
-{
-    unsigned char send_buf[] = "abcde\n";
-    // unsigned char recv_buf[100];
-
-    int sockfd = connect_controller(6788);
-    if( sockfd < 0)
-    {
-        printf("connect controller failed\n");
-    }
-
-    while(1)
-    {
-        if(talk_controller(sockfd, send_buf) == 0)
+        // printf("%d\n", sizeof(send_buf));
+        if (send(sockfd, send_buf, sizeof(send_buf), 0) < 0)
         {
-            printf("talk controller success\n");
+            printf("send failed\n");
+            return -1;
+        }
+
+        // recv
+        if (recv(sockfd, recv_buf, sizeof(recv_buf), 0) > 0)
+        {
+            printf("recv: %s\n", recv_buf);
         }
     }
     close(sockfd);
