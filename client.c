8d49d12a76b459b02476b2095fee439987c8d496
Hui Wu <wuhui2014@stu.xjtu.edu.cn>
2019-09-25 15:13:32 +0800

    correct a bug

--- client.c
+++ client.c
@@ -6,12 +6,13 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-int main() 
+
+int connect_controller(int port)
 {
     struct sockaddr_in my_addr;
     // memset(&my_addr, 0, sizeof(my_addr));
     my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(2000);
+    my_addr.sin_port = htons(port);
     my_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 
@@ -25,27 +26,52 @@ int main()
     }
 
     // connect
-    if(connect(sockfd, (struct socketaddr*)&my_addr, sizeof(my_addr)) < 0)
+    if(connect(sockfd, (struct sockaddr*)&my_addr, sizeof(my_addr)) < 0)
     {
         printf("connect failed\n");
         return -1;
     }
+    return sockfd;
+}
 
-    unsigned char send_buf[] = "abcde\n";
-    unsigned char recv_buf[100];
-    //send
-    if(send(sockfd, send_buf, sizeof(send_buf), 0) < 0)
+int talk_controller(int sockfd, unsigned char* send_msg)
+{
+    if(send(sockfd, send_msg, sizeof(send_msg), 0) < 0)
     {
         printf("send failed\n");
         return -1;
     }
 
     // recv
-    if(recv(sockfd, recv_buf, sizeof(recv_buf), 0) > 0)
+    unsigned char recv_msg[100];
+    if(recv(sockfd, recv_msg, sizeof(recv_msg), 0) > 0)
     {
-        printf("recv: %s\n", recv_buf);
+        printf("recv: %s\n", recv_msg);
+    }else{
+        printf("recv failed\n");
     }
 
+    return 0;
+}
+
+int main() 
+{
+    unsigned char send_buf[] = "abcde\n";
+    // unsigned char recv_buf[100];
+
+    int sockfd = connect_controller(6788);
+    if( sockfd < 0)
+    {
+        printf("connect controller failed\n");
+    }
+
+    while(1)
+    {
+        if(talk_controller(sockfd, send_buf) == 0)
+        {
+            printf("talk controller success\n");
+        }
+    }
     close(sockfd);
     return 0;
 }
